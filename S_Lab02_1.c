2c3275eee25c28620366af3132dbe082354c4740
yoonhee26 <ellaella4934@sookmyung.ac.kr>
2026-07-26 01:27:00 +0900

    S_Lab02

---
+++ S_Lab02_1.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+    int age;
+    scanf("%d", &age);
+
+    if (age >= 20) {
+        printf("adult\n");
+    }
+    else {
+        printf("%d years later\n", 20 - age);
+    }
+
+    return 0;
+}
